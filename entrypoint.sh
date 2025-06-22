173c3997642d378f17e7cb93262432b364957615
Juls <luigfmp@gmail.com>
2025-06-22 14:31:40 -0500

    refactor: :fire: Update Odoo configuration and docker-compose settings; change database credentials and ports

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,12 @@ set -e
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo18@2024'}}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='haui'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='haui-psw'}}}
 
 # install python packages
-# pip3 install pip --upgrade                # may cause errors
+# pip3 install pip --upgrade
+# may cause errors
 pip3 install -r /etc/odoo/requirements.txt
 
 # sed -i 's|raise werkzeug.exceptions.BadRequest(msg)|self.jsonrequest = {}|g' /usr/lib/python3/dist-packages/odoo/http.py
